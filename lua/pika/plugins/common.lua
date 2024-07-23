724c5d23467187981250724f278bee5ffd2aa1a4
Chi-Liang Liu <liangtaiwan1230@gmail.com>
2024-07-23 14:43:57 +0900

    add treesitter and update some package

--- lua/pika/plugins/common.lua
+++ lua/pika/plugins/common.lua
@@ -1,32 +1,16 @@
 return {
   { "ntpeters/vim-better-whitespace", event = "VeryLazy" },
-  { "kylechui/nvim-surround", event = "VeryLazy" },
   { "tpope/vim-abolish", event = "VeryLazy" },
-  {
-    "numToStr/Comment.nvim",
-    event = "VeryLazy",
-    config = true,
-  },
+  { "kylechui/nvim-surround", event = "VeryLazy", config = true },
+  { "numToStr/Comment.nvim", event = "VeryLazy", config = true },
   {
     "lukas-reineke/indent-blankline.nvim",
     event = "VeryLazy",
-    config = true,
+    main = "ibl",
     opts = {
-      buftype_exclude = { "terminal", "help" },
-      show_first_indent_level = false,
+      exclude = {
+        buftypes = { "terminal", "help" },
+      }
     },
-  },
-  {
-    "ojroques/nvim-osc52",
-    init = function()
-      vim.api.nvim_create_autocmd("TextYankPost", {
-        callback = function()
-          -- Use OSC52 when + register is used
-          if vim.v.event.operator == "y" and vim.v.event.regname == "+" then
-            require("osc52").copy_register("+")
-          end
-        end,
-      })
-    end,
   },
 }
